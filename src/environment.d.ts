a880cb0e370deee4785603223e979c22ee71aeb4
Connorbelez <connor.belez@gmail.com>
2026-06-20 06:42:34 -0400

    fixing vercel deploy errors

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -2,7 +2,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PAYLOAD_SECRET: string
+      POSTGRES_URL: string
       DATABASE_URL: string
+      BLOB_READ_WRITE_TOKEN: string
+      CRON_SECRET: string
       NEXT_PUBLIC_SERVER_URL: string
       VERCEL_PROJECT_PRODUCTION_URL: string
     }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -63,22 +63,10 @@ export default buildConfig({
       connectionString: process.env.POSTGRES_URL || '',
     },
   }),
+  folders: {
+    slug: 'payload-folders',
+  },
   collections: [
-    {
-      slug: 'folders',
-      folders: true,
-      admin: {
-        useAsTitle: 'name',
-      },
-      fields: [
-        {
-          name: 'name',
-          type: 'text',
-          required: true,
-          label: 'Folder Name',
-        },
-      ],
-    },
     Pages,
     Posts,
     Media,
@@ -86,7 +74,15 @@ export default buildConfig({
     Users,
   ],
   cors: [getServerSideURL()].filter(Boolean),
-  plugins: [...plugins],
+  plugins: [
+    ...plugins,
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+    }),
+  ],
   globals: [Header, Footer],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
@@ -111,12 +107,4 @@ export default buildConfig({
     },
     tasks: [],
   },
-  storage: [
-    vercelBlobStorage({
-      collections: {
-        media: true,
-      },
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
-    }),
-  ],
 })
